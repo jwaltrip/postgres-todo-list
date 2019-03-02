d0a72913537fcaa38b9bbc1d60e595369025d0c3
Jake Waltrip <jake.waltrip@gmail.com>
2019-03-01 18:56:39 -0600

    finished static structure

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -19,41 +19,49 @@ class TodoList extends Component {
         <Container>
           <div className="d-flex justify-content-center">
             <ListGroup className="text-left w-50">
-              <ListGroupItem>
+              <ListGroupItem className="todo-form-header">
                 <Form>
                   <FormGroup>
-                    <h6 className="text-center">Add To-Do</h6>
-                    <Input type="text" name="text" id="addTodo" placeholder="Add an item to the list..." />
-                    <Button color="success">Add Todo</Button>
+                    <h5 className="text-center">Add To-Do</h5>
+                    <div className="add-todo-form">
+                      <Input type="text" name="text" id="addTodo" placeholder="Add an item to the list..." />
+                      <Button color="success">Add Todo</Button>
+                    </div>
                   </FormGroup>
                 </Form>
               </ListGroupItem>
               
               <ListGroupItem>
-                Jerk off
-                <span className="float-right">
-                  <Button color="danger" size="sm">
-                    Remove
-                  </Button>
-                </span>
+                <div className="d-flex flex-row align-items-center">
+                  <div className="todo-text">Jerk off</div>
+                  <div className="todo-remove-btn">
+                    <Button color="danger" size="sm">
+                      Remove
+                    </Button>
+                  </div>
+                </div>
               </ListGroupItem>
-
+  
               <ListGroupItem>
-                Wait 15 min
-                <span className="float-right">
-                  <Button color="danger" size="sm">
-                    Remove
-                  </Button>
-                </span>
+                <div className="d-flex flex-row align-items-center">
+                  <div className="todo-text">Wait 15 min</div>
+                  <div className="todo-remove-btn">
+                    <Button color="danger" size="sm">
+                      Remove
+                    </Button>
+                  </div>
+                </div>
               </ListGroupItem>
-
+  
               <ListGroupItem>
-                Repeat
-                <span className="float-right">
-                  <Button color="danger" size="sm">
-                    Remove
-                  </Button>
-                </span>
+                <div className="d-flex flex-row align-items-center">
+                  <div className="todo-text">Repeat</div>
+                  <div className="todo-remove-btn">
+                    <Button color="danger" size="sm">
+                      Remove
+                    </Button>
+                  </div>
+                </div>
               </ListGroupItem>
             </ListGroup>
           </div>
